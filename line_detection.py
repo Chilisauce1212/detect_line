e5d5ea24b31755307d80298240e08b0d44c08e3c
chilisauce <chilisauce1212@outlook.com>
2021-09-28 09:59:29 +0800

    9/28

--- line_detection.py
+++ line_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 
 def Slope(line):
@@ -34,21 +35,31 @@ def least_square(lines):
 
 
 def edge(path):
+    name = path.split(".")[0]
+    if(os.path.exists(name) != True):
+        os.mkdir(name)
     grey = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     edge = cv2.Canny(grey, 192, 433)
-    cv2.imwrite("edge.jpg", edge)
-    return edge
+    cv2.imwrite("{}/edge.jpg".format(name), edge)
 
 
-def ROI(edge_img):
+
+def ROI(path):
+    name = path.split(".")[0]
+    if(os.path.exists(name) != True):
+        os.mkdir(name)
+    edge_img = cv2.imread("{}/edge.jpg".format(name), cv2.IMREAD_ANYCOLOR)
     zero = np.zeros_like(edge_img)
     mask = cv2.fillPoly(zero, np.array([[[0, 558], [0, 508], [500, 360], [600, 360], [1080, 558]]]), 255)
     ROI = cv2.bitwise_and(edge_img, mask)
-    cv2.imwrite("ROI.jpg", ROI)
-    return ROI
+    cv2.imwrite("{}/ROI.jpg".format(name), ROI)
 
 
-def hough(ROI_img):
+def hough(path):
+    name = path.split(".")[0]
+    if(os.path.exists(name) != True):
+        os.mkdir(name)
+    ROI_img = cv2.imread("{}/ROI.jpg".format(name), cv2.IMREAD_ANYCOLOR)
     lines = cv2.HoughLinesP(ROI_img, 1, np.pi / 180, 30, 40)
     left_lines = [line for line in lines if Slope(line) < 0]
     right_lines = [line for line in lines if Slope(line) > 0]
@@ -60,13 +71,17 @@ def hough(ROI_img):
 
 
 def draw(path):
+    name = path.split(".")[0]
+    if(os.path.exists(name) != True):
+        os.mkdir(name)
     raw = cv2.imread(path, cv2.IMREAD_ANYCOLOR)
-    edge_img = edge(path)
-    ROI_img = ROI(edge_img)
-    left_line, right_line = hough(ROI_img)
+    cv2.imwrite("{}/raw.jpg".format(name), raw)
+    edge(path)
+    ROI(path)
+    left_line, right_line = hough(path)
     cv2.line(raw, tuple(left_line[0]), tuple(left_line[1]), (255, 0, 0), 3)
     cv2.line(raw, tuple(right_line[0]), tuple(right_line[1]), (255, 0, 0), 3)
-    cv2.imwrite("draw.jpg", raw)
+    cv2.imwrite("{}/draw.jpg".format(name), raw)
 
 
 if __name__ == "__main__":
